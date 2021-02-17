0342752fa8d423c7b28c34f0b04723df32c7682a
doyeon <doyeon311@gmail.com>
2021-02-17 22:45:13 +0900

    생성: Post 관련 Controller

---
+++ src/kr/co/rgrg/user/post/controller/PostController.java
@@ -0,0 +1,45 @@
+package kr.co.rgrg.user.post.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.co.rgrg.user.post.vo.ModifyPostVO;
+import kr.co.rgrg.user.post.vo.PostVO;
+
+@Controller
+public class PostController {
+	
+	@RequestMapping(value="/", method= RequestMethod.GET)
+	public String writePostForm() {
+		
+		return "";
+	}
+	
+	@RequestMapping(value="/", method= RequestMethod.GET)
+	public String saveNewPost(PostVO pVO, HttpSession session) {
+		
+		return "";
+	}
+	
+	@RequestMapping(value="/", method= RequestMethod.GET)
+	public String saveModifyPost(ModifyPostVO mpVO, HttpSession session) {
+		
+		return "";
+	}
+	
+	@RequestMapping(value="/", method= RequestMethod.GET)
+	public String getModifyPost(String postNum, Model model,HttpSession session) {
+		
+		return "";
+	}
+	
+	@RequestMapping(value="/", method= RequestMethod.GET)
+	public String cancelPost(String postNum) {
+		
+		return "";
+	}
+}
