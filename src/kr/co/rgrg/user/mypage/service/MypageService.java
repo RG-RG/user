8354d9e2c88e4641e89b29c5c7f4006a77341cf3
doyeon <doyeon311@gmail.com>
2021-02-17 22:46:07 +0900

    생성: Mypage 관련 Service

---
+++ src/kr/co/rgrg/user/mypage/service/MypageService.java
@@ -0,0 +1,76 @@
+package kr.co.rgrg.user.mypage.service;
+
+import java.util.List;
+
+import kr.co.rgrg.user.mypage.domain.MypageDomain;
+import kr.co.rgrg.user.mypage.domain.StatisticsDomain;
+import kr.co.rgrg.user.mypage.vo.PassChkVO;
+import kr.co.rgrg.user.mypage.vo.UpdateBlogTitleVO;
+import kr.co.rgrg.user.mypage.vo.UpdateEmailVO;
+import kr.co.rgrg.user.mypage.vo.UpdatePassVO;
+import kr.co.rgrg.user.mypage.vo.UpdateProfileImgVO;
+import kr.co.rgrg.user.mypage.vo.UpdateProfileVO;
+import kr.co.rgrg.user.mypage.vo.UpdateWebsiteVO;
+
+public class MypageService {
+	
+	public MypageDomain getMypage(String id) {
+		MypageDomain mDomain = null;
+		
+		return mDomain;
+	}
+	
+	public boolean modifyProfileImg(UpdateProfileImgVO upiVO) {
+		boolean result = false;
+		
+		return result;
+	}
+	
+	public boolean modifyProfile(UpdateProfileVO upVO) {
+		boolean result = false;
+		
+		return result;
+	}
+	
+	public boolean modifyBlogTitle(UpdateBlogTitleVO ubtVO) {
+		boolean result = false;
+		
+		return result;
+	}
+	
+	public boolean modifyWebsite(UpdateWebsiteVO uwVO) {
+		boolean result = false;
+		
+		return result;
+	}
+	
+	public boolean modifyEmail(UpdateEmailVO ueVO) {
+		boolean result = false;
+		
+		return result;
+	}
+	
+	public boolean removeMemberChk(PassChkVO pcVO) {
+		boolean flag = false;
+		
+		return  flag;
+	}
+	
+	public boolean modifyPassChk(PassChkVO pcVO) {
+		boolean flag = false;
+		
+		return flag;
+	}
+	
+	public boolean modifyPass(UpdatePassVO upVO) {
+		boolean flag = false;
+		
+		return flag;
+	}
+	
+	public List<StatisticsDomain> getStatistics(String url) {
+		List<StatisticsDomain> list = null;
+		
+		return list;
+	}
+}
