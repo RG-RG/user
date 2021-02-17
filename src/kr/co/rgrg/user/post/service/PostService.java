e89061c23b4bf93bb05c18bf03ca5f965a21444b
doyeon <doyeon311@gmail.com>
2021-02-17 22:41:22 +0900

    생성: Post 관련 Service

---
+++ src/kr/co/rgrg/user/post/service/PostService.java
@@ -0,0 +1,28 @@
+package kr.co.rgrg.user.post.service;
+
+import kr.co.rgrg.user.post.domain.PostDomain;
+import kr.co.rgrg.user.post.vo.ModifyPostVO;
+import kr.co.rgrg.user.post.vo.PostVO;
+
+public class PostService {
+	
+	public boolean savePost(PostVO pVO) {
+		
+		return false;
+	}
+	
+	public boolean modifyPost(ModifyPostVO mpVO) {
+		
+		return false;
+	}
+	
+	public PostDomain displayPost(int post_num) {
+		PostDomain postdomain = null;
+		return postdomain;
+	}
+	
+	public boolean deletePost(int post_num) {
+		
+		return false;
+	}
+}
