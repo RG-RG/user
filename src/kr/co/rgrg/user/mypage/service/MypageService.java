45542de00e2fc08e3eb27d0a423e0825e16c4717
doyeon <doyeon311@gmail.com>
2021-03-01 23:07:20 +0900

    추가: MypageService / 프로필 이미지, 닉네임, 상태메세지를 변경한 결과를 Controller로 보내줌

--- src/kr/co/rgrg/user/mypage/service/MypageService.java
+++ src/kr/co/rgrg/user/mypage/service/MypageService.java
@@ -15,6 +15,11 @@ import kr.co.rgrg.user.mypage.vo.UpdateWebsiteVO;
 
 public class MypageService {
 	
+	/**
+	 * 마이페이지 첫화면 데이터를 DAO에서 받아서 Controller로 보내는 일
+	 * @param id
+	 * @return
+	 */
 	public MypageDomain getMypage(String id) {
 		MypageDomain md = null;
 		
@@ -24,20 +29,43 @@ public class MypageService {
 		return md;
 	}
 	
+	/**
+	 * DAO를 사용해서 프로필이미지를 변경하고 결과를 Controller로 보내는 일
+	 * 
+	 * @param upiVO
+	 * @return
+	 */
 	public boolean modifyProfileImg(UpdateProfileImgVO upiVO) {
 		boolean result = false;
+		
+		MypageDAO mDAO = MypageDAO.getInstance();
+		if(mDAO.updateProfileImg(upiVO) > 0) {
+			result = true;
+		}
 		
 		return result;
 	}
 	
+	/**
+	 * DAO를 사용해서 닉네임, 상태메세지를 변경하고 결과를 Controller로 보내는 일
+	 * @param upVO
+	 * @return
+	 */
 	public boolean modifyProfile(UpdateProfileVO upVO) {
 		boolean result = false;
+		
+		MypageDAO mDAO = MypageDAO.getInstance();
+		if(mDAO.updateProfile(upVO) > 0) {
+			result = true;
+		}
 		
 		return result;
 	}
 	
 	public boolean modifyBlogTitle(UpdateBlogTitleVO ubtVO) {
 		boolean result = false;
+		
+		
 		
 		return result;
 	}
