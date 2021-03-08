89322aa8966f9121294399c4827f311f13c1129b
zwooo96 <dlwldn385@naver.com>
2021-03-08 22:46:18 +0900

    add LoginDomain

---
+++ src/kr/co/rgrg/user/member/domain/LoginDomain.java
@@ -0,0 +1,23 @@
+package kr.co.rgrg.user.member.domain;
+
+public class LoginDomain {
+	
+	private String id, pass;
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+}//LoginDomain
