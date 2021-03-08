d1770c2f5f75ee6ff3b74be6a32de77828622dc3
doyeon <doyeon311@gmail.com>
2021-03-09 01:24:59 +0900

    추가: PostController / view 추가

--- src/kr/co/rgrg/user/post/controller/PostController.java
+++ src/kr/co/rgrg/user/post/controller/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.co.rgrg.user.post.service.PostService;
 import kr.co.rgrg.user.post.vo.ModifyPostVO;
 import kr.co.rgrg.user.post.vo.PostVO;
 
@@ -16,30 +17,43 @@ public class PostController {
 	@RequestMapping(value="/post/post_form.do", method= RequestMethod.GET)
 	public String writePostForm() {
 		
-		return "";
+		PostService ps = new PostService();
+		
+		
+		return "post/post";
 	}
 	
 	@RequestMapping(value="/post/new_post.do", method= RequestMethod.GET)
 	public String saveNewPost(PostVO pVO, HttpSession session) {
 		
-		return "";
+		PostService ps = new PostService();
+		
+		
+		return "post/new";
 	}
 	
 	@RequestMapping(value="/post/save_modify_post.do", method= RequestMethod.GET)
 	public String saveModifyPost(ModifyPostVO mpVO, HttpSession session) {
 		
-		return "";
+		PostService ps = new PostService();
+		
+		
+		return "post/save_modify";
 	}
 	
 	@RequestMapping(value="/post/get_modify_post.do", method= RequestMethod.GET)
 	public String getModifyPost(String postNum, Model model,HttpSession session) {
 		
-		return "";
+		PostService ps = new PostService();
+		
+		return "post/get_modify";
 	}
 	
 	@RequestMapping(value="/post/cancel.do", method= RequestMethod.GET)
 	public String cancelPost(String postNum) {
 		
-		return "";
+		PostService ps = new PostService();
+		
+		return "post/cancel";
 	}
 }
