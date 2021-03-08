d79cf3071780899ab2d3080533d3688ecc6a686e
doyeon <doyeon311@gmail.com>
2021-03-09 01:21:44 +0900

    추가: PostService / 게시글 저장, 수정, 조회, 삭제

--- src/kr/co/rgrg/user/post/service/PostService.java
+++ src/kr/co/rgrg/user/post/service/PostService.java
@@ -1,28 +1,71 @@
 package kr.co.rgrg.user.post.service;
 
+
+import kr.co.rgrg.user.post.dao.PostDAO;
 import kr.co.rgrg.user.post.domain.PostDomain;
 import kr.co.rgrg.user.post.vo.ModifyPostVO;
 import kr.co.rgrg.user.post.vo.PostVO;
 
 public class PostService {
 	
+	/**
+	 * 게시글 저장
+	 * @param pVO
+	 * @return
+	 */
 	public boolean savePost(PostVO pVO) {
+		boolean result = false;
 		
-		return false;
+		PostDAO pDAO = PostDAO.getInstance();
+		if(pDAO.insertPost(pVO) > 0) {
+			result = true;
+		}
+		
+		return result;
 	}
 	
+	/**
+	 * 게시글 수정
+	 * @param mpVO
+	 * @return
+	 */
 	public boolean modifyPost(ModifyPostVO mpVO) {
+		boolean result = false;
 		
-		return false;
+		PostDAO pDAO = PostDAO.getInstance();
+		pDAO.updatePost(mpVO);
+		pDAO.deleteTag(mpVO.getPost_num());
+		pDAO.insertTag(mpVO);
+		
+		return result;
 	}
 	
-	public PostDomain displayPost(int post_num) {
-		PostDomain postdomain = null;
-		return postdomain;
+	/**
+	 * 게시글 조회
+	 * @param post_num
+	 * @return
+	 */
+	public PostDomain displayPost(String post_num) {
+		PostDomain pDomain = null;
+		
+		PostDAO pDAO = PostDAO.getInstance();
+		pDomain = pDAO.selectPost(post_num);
+		
+		return pDomain;
 	}
 	
-	public boolean deletePost(int post_num) {
+	/**
+	 * 게시글 삭제
+	 * @param post_num
+	 * @return
+	 */
+	public boolean deletePost(String post_num) {
+		boolean result = false;
 		
-		return false;
+		PostDAO pDAO = PostDAO.getInstance();
+		if(pDAO.deletePost(post_num) + pDAO.deleteTag(post_num) > 0) {
+			result = true;
+		}
+		return result;
 	}
 }
