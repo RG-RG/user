d728b168b979dbed5150af5a982488d3a18d1f2b
doyeon <doyeon311@gmail.com>
2021-03-23 23:19:50 +0900

    추가: post / controller / 로그인 세션 값 확인하기

--- src/kr/co/rgrg/user/post/controller/PostController.java
+++ src/kr/co/rgrg/user/post/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
@@ -17,6 +18,7 @@ import kr.co.rgrg.user.post.service.PostService;
 import kr.co.rgrg.user.post.vo.ModifyPostVO;
 import kr.co.rgrg.user.post.vo.PostVO;
 
+@SessionAttributes("id")
 @Controller
 public class PostController {
 	private static final Logger logger = LoggerFactory.getLogger(PostService.class);
@@ -34,20 +36,28 @@ public class PostController {
 	 */
 	@RequestMapping(value="/post/post_form.do", method= RequestMethod.GET)
 	public String writePostForm(HttpSession session, Model model) {
+		String id = (String)session.getAttribute("id");
 		
-		model.addAttribute("temp_post_flag", new PostService().searchPublishPost("user1"));
+		if (id != null) {
+			model.addAttribute("temp_post_flag", new PostService().searchPublishPost(id));			
+		}
+		model.addAttribute("id", id);
+
 		return "post/edit";
 	}
 	
 	@RequestMapping(value="/post/temp_post_form.do", method= RequestMethod.POST)
 	public String getTempPostForm(Model model, HttpSession session) {
+		String id = (String)session.getAttribute("id");
 		
-		PostService ps = new PostService();
-		String post_num = ps.searchPublishPost("user1");
-		PostDomain pdomain = ps.searchEditPost(post_num);
-		System.out.println(post_num);
-		model.addAttribute("post_data", pdomain);
-		
+		if(id != null) {
+			PostService ps = new PostService();
+			String post_num = ps.searchPublishPost(id);
+			PostDomain pdomain = ps.searchEditPost(post_num);
+			System.out.println(post_num);
+			model.addAttribute("post_data", pdomain);			
+		}
+		model.addAttribute("id", id);
 		return "post/edit";
 	}
 	
@@ -69,29 +79,34 @@ public class PostController {
 	 */
 	@RequestMapping(value="/post/new_post.do", method= RequestMethod.POST)
 	public String saveNewPost(PostVO pVO, HttpSession session, MultipartFile thumbnail_img, Model model) throws Exception {
-		pVO.setId("user1");
-		PostService ps = new PostService();
+		String id = (String)session.getAttribute("id");
 		
-		String upload_result = "";
-		boolean post_result = false;
-		try {
-			if(thumbnail_img != null) {
-				upload_result = ps.saveFile(thumbnail_img);
-				pVO.setThumbnail(upload_result);				
+		if(id != null) {
+			pVO.setId("user1");
+			PostService ps = new PostService();
+			
+			String upload_result = "";
+			boolean post_result = false;
+			try {
+				if(thumbnail_img != null) {
+					upload_result = ps.saveFile(thumbnail_img);
+					pVO.setThumbnail(upload_result);				
+				}
+				
+				post_result = ps.savePost(pVO);
+				System.out.println(post_result);
+			} catch (NullPointerException e) {
+				upload_result = "no_file";
 			}
 			
-			post_result = ps.savePost(pVO);
-			System.out.println(post_result);
-		} catch (NullPointerException e) {
-			upload_result = "no_file";
+			if(post_result && upload_result != null) {
+				model.addAttribute("posting_result", "success");
+			} else {
+				model.addAttribute("posting_result", "fail");
+			}
 		}
 		
-		if(post_result && upload_result != null) {
-			model.addAttribute("posting_result", "success");
-		} else {
-			model.addAttribute("posting_result", "fail");
-		}
-		return "main/main";
+		return "redirect:/rgrg/main/main";
 	}
 	
 
@@ -104,10 +119,14 @@ public class PostController {
 	 */
 	@RequestMapping(value="/post/get_modify_post.do", method= RequestMethod.GET)
 	public String getModifyPost(String post_num, Model model,HttpSession session) {
+		String id = (String)session.getAttribute("id");
 		
-		PostService ps = new PostService();
-		PostDomain pdomain = ps.searchEditPost(post_num);
-		model.addAttribute("post_data", pdomain);
+		if(id != null) {
+			PostService ps = new PostService();
+			PostDomain pdomain = ps.searchEditPost(post_num);
+			model.addAttribute("post_data", pdomain);			
+		}
+		model.addAttribute("id", id);
 		
 		return "post/edit";
 	}
@@ -120,31 +139,36 @@ public class PostController {
 	 */
 	@RequestMapping(value="/post/save_modify_post.do", method= RequestMethod.POST)
 	public String saveModifyPost(ModifyPostVO mpVO, MultipartFile thumbnail_img, HttpSession session, Model model) {
-		mpVO.setId("user1");
-		PostService ps = new PostService();
-		// result에 따라 어디로 이동할지
+		String id = (String)session.getAttribute("id");
 		
-		String upload_result = "";
-		boolean post_result = false;
-		try {
-			if(thumbnail_img != null) {
-				upload_result = ps.saveFile(thumbnail_img);
-				mpVO.setThumbnail(upload_result);				
+		if (id != null) {
+			mpVO.setId("user1");
+			PostService ps = new PostService();
+			// result에 따라 어디로 이동할지
+			
+			String upload_result = "";
+			boolean post_result = false;
+			try {
+				if(thumbnail_img != null) {
+					upload_result = ps.saveFile(thumbnail_img);
+					mpVO.setThumbnail(upload_result);				
+				}
+				
+				post_result = ps.modifyPost(mpVO);
+				System.out.println(post_result);
+			} catch (NullPointerException e) {
+				upload_result = "no_file";
 			}
 			
-			post_result = ps.modifyPost(mpVO);
-			System.out.println(post_result);
-		} catch (NullPointerException e) {
-			upload_result = "no_file";
+			if(post_result && upload_result != null) {
+				model.addAttribute("posting_result", "success");
+			} else {
+				model.addAttribute("posting_result", "fail");
+			}
+			
 		}
 		
-		if(post_result && upload_result != null) {
-			model.addAttribute("posting_result", "success");
-		} else {
-			model.addAttribute("posting_result", "fail");
-		}
-		
-		return "main/main";
+		return "redirect:/rgrg/main/main";
 	}
 	
 	@RequestMapping(value="/post/cancel.do", method= RequestMethod.GET)
